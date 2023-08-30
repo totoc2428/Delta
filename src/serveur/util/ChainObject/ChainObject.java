14024339e7a0b9d07d8a23c0f4f5d118839e0c5f
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2023-08-30 22:55:51 +0200

    ajout de block et des class Node

--- src/serveur/util/ChainObject/ChainObject.java
+++ src/serveur/util/ChainObject/ChainObject.java
@@ -1,5 +1,10 @@
 package serveur.util.ChainObject;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import serveur.util.data.DataCSV;
 import serveur.util.security.Key;
 
 public abstract class ChainObject {
@@ -9,11 +14,21 @@ public abstract class ChainObject {
         this.signature = signature;
     }
 
+    public ChainObject(String signature, String privatekey) {
+        if (Paths.get(signature + ".csv").toFile().exists()) {
+            ArrayList<String> list = DataCSV.filetoStringArrayList(
+                    Paths.get("chainObject" + File.separator + "" + signature + ".csv").toFile());
+
+        } else {
+            // ..
+        }
+    }
+
     public Key getSignature() {
         return signature;
     }
 
     public String toWriteFormat() {
-        return signature + ";";
+        return signature.getPublickey();
     }
 }
