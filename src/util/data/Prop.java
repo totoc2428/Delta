8e6f8d8d9e6bcb6a10f66cd19ef56425dcfcad9a
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-01-14 01:30:45 +0100

    ajout de Folder et doc pour les boites outils Data

--- src/util/data/Prop.java
+++ src/util/data/Prop.java
@@ -9,6 +9,14 @@ import java.util.Properties;
 import util.exception.util.data.DataPropWriteConfigException;
 
 public class Prop {
+    /**
+     * This method take a file where are saved a properties type and read it to a
+     * usable format.
+     * 
+     * @param fileName the file Name who you want to read.
+     * @return A {@link Properties} instance contain the key-value pair saved in the
+     *         file.
+     */
     public static Properties read(File fileName) {
         Properties properties = new Properties();
         try {
@@ -21,9 +29,16 @@ public class Prop {
         return properties;
     }
 
+    /**
+     * This method take a {@link Properties} instance to save it in a regular file
+     * (.conf).
+     * 
+     * @param properties the Properties instance who you want to read.
+     * @param filePath   the destination sources : where are save your file.
+     */
     public static void writeConfig(Properties properties, String filePath) {
         try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
-            properties.store(fileOutputStream, "Configuration File");
+            properties.store(fileOutputStream, "");
         } catch (IOException e) {
             new DataPropWriteConfigException();
         }
