08002e3f10c4dc2aeed012edac7b247452d88b3f
Charles COUDE <charles.coude.etu@univ-lille.fr>
2024-03-28 13:29:51 +0100

    test vert pour le dic

--- src/test/model/dao/DataManagerTest.java
+++ src/test/model/dao/DataManagerTest.java
@@ -3,8 +3,13 @@ package test.model.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.net.Socket;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
@@ -67,15 +72,109 @@ public class DataManagerTest {
     }
 
     @Test
-    public void testReadAFile() {
-        Properties properties = DataManager.read("./ressources/init/init.conf");
+    public void testReadAnExistingFile() {
+        Properties properties = DataManager.read("./ressources/test/test_properties.conf");
 
         assertFalse(properties.isEmpty());
     }
 
     ////// TEXT FILE
     @Test
-    public void testTextFileToStringArrayList() {
+    public void testTextFileToStringArrayListNullFile() {
 
+        ArrayList<String> textLines = DataManager.textFileToStringArrayList("");
+
+        assertNull(textLines);
+    }
+
+    @Test
+    public void testTextFileToStringArrayListNullFileWithPath() {
+
+        ArrayList<String> textLines = DataManager.textFileToStringArrayList(Paths.get("").toFile());
+
+        assertTrue(textLines.isEmpty());
+    }
+
+    @Test
+    public void testTextFileToStringArrayListExistingFile() {
+        ArrayList<String> textLines = DataManager.textFileToStringArrayList("./ressources/test/test_line_text.txt");
+
+        assertFalse(textLines.isEmpty());
+
+        assertNotNull(textLines.get(0));
+        assertNotNull(textLines.get(1));
+
+        assertEquals(textLines.get(0), "line1");
+        assertEquals(textLines.get(1), "line2");
+    }
+
+    @Test
+    public void testSave() {
+        Properties savedProperties = new Properties();
+
+        savedProperties.setProperty("saved1", "saved1");
+        savedProperties.setProperty("saved2", "saved2");
+
+        String defaultSrcPath = DataManager.srcPath;
+        DataManager.srcPath = "";
+
+        String outPutPath = "./ressources/test/saved_properties.test.conf";
+
+        DataManager.srcPath = defaultSrcPath;
+
+        DataManager.save(savedProperties, outPutPath);
+
+        assertTrue(Paths.get(outPutPath).toFile().isFile());
+        assertTrue(Paths.get(outPutPath).toFile().exists());
+
+    }
+
+    @Test
+    public void testFileExist() {
+        String testFile = "./ressources/test/test_line_text.txt";
+
+        assertTrue(DataManager.fileExist(testFile));
+        assertFalse(DataManager.fileExist(""));
+    }
+
+    @Test
+    public void testFileIsDirectory() {
+        String testDirectory = "./ressources/test";
+        String testFile = "./ressources/test/test_line_text.txt";
+
+        assertTrue(DataManager.fileIsDirectory(testDirectory));
+        assertFalse(DataManager.fileIsDirectory(testFile));
+    }
+
+    @Test
+    public void testObjectCollectionToAString() {
+        ArrayList<Object> objects = new ArrayList<>();
+
+        String separedListSpace = DataManager.SAVED_LIST_SPACE;
+
+        String savedResult = separedListSpace + "obj1" + separedListSpace + "obj2" + separedListSpace + "obj3";
+        objects.add("obj1");
+        objects.add("obj2");
+        objects.add("obj3");
+
+        assertEquals(savedResult, DataManager.objectCollectionToAString(objects));
+    }
+
+    @Test
+    public void testObjectHashMapToString() {
+        HashMap<Object, Object> dic = new HashMap<Object, Object>();
+
+        dic.put("tag1", "key1");
+        dic.put("tag2", "key2");
+        dic.put("tag3", "key3");
+
+        String separedListSpace = DataManager.SAVED_LIST_SPACE;
+        String separedDicSpace = DataManager.SAVED_DIC_SPACE;
+
+        String savedResult = separedListSpace
+                + "key1" + separedListSpace + "key2" + separedListSpace + "key3" + separedDicSpace + separedListSpace
+                + "tag1" + separedListSpace + "tag2" + separedListSpace + "tag3";
+
+        assertEquals(savedResult, DataManager.objectHashMapToString(dic));
     }
 }
