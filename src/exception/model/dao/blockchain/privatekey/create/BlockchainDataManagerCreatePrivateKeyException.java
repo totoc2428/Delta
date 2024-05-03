92b5e9b759d168167500199bf62b6a71a2f3f258
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-05-03 13:48:24 +0200

    modification architecture BlockchainDataManagerCreatePrivateKeyException ( class enfant de BlockchainDataManagerCreatePrivateKeyException

--- src/exception/model/dao/blockchain/privatekey/create/BlockchainDataManagerCreatePrivateKeyException.java
+++ src/exception/model/dao/blockchain/privatekey/create/BlockchainDataManagerCreatePrivateKeyException.java
@@ -1,8 +1,8 @@
 package exception.model.dao.blockchain.privatekey.create;
 
-import exception.model.dao.blockchain.BlockchainDataManagerException;
+import exception.model.dao.blockchain.privatekey.PrivateKeyBlockchainDataManagerException;
 
-public abstract class BlockchainDataManagerCreatePrivateKeyException extends BlockchainDataManagerException {
+public abstract class BlockchainDataManagerCreatePrivateKeyException extends PrivateKeyBlockchainDataManagerException {
     public BlockchainDataManagerCreatePrivateKeyException(String message, String code) {
         super(message, code);
     }
