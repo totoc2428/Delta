879f209efaf23c1f1fab05bd233206d2090582f5
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-06-19 16:37:43 +0200

    ajout de la verification disponibilité du language

--- src/util/tool/Primary.java
+++ src/util/tool/Primary.java
@@ -1,5 +1,44 @@
 package util.tool;
 
-public abstract class Primary {
+import java.util.Properties;
 
+import exception.system.util.data.PropertieReadingSystemException;
+import exception.system.util.language.LangueageMessageNotFoundSystemExcetion;
+import util.data.DataManager;
+
+public abstract class Primary {
+    private static final Properties INIT_PROPERTIES = initProperties();
+
+    private static String SystemlanguageValue = INIT_PROPERTIES.getProperty("SYSTEM_LANGUAGE");
+
+    public static final String DATA_MANAGER_INIT_FILE_NAME = INIT_PROPERTIES.getProperty("INIT_DATAMANAGER");
+
+    public static void setSystemlanguageValue(String systemlanguageValue)
+            throws LangueageMessageNotFoundSystemExcetion {
+        if (isLanguageAviable(systemlanguageValue)) {
+            SystemlanguageValue = systemlanguageValue.toUpperCase();
+        } else {
+            throw new LangueageMessageNotFoundSystemExcetion();
+        }
+
+    }
+
+    private static boolean isLanguageAviable(String systemLanguageCode) {
+        boolean aviable = false;
+
+        return aviable;
+    }
+
+    public static String getSystemlanguageValue() {
+        return SystemlanguageValue;
+    }
+
+    private static Properties initProperties() {
+        try {
+            return DataManager.readAFile("./ressources/init.conf");
+        } catch (PropertieReadingSystemException e) {
+            e.show();
+            return null;
+        }
+    }
 }
