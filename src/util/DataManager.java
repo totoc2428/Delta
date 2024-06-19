5be83b3ad1faafacd70e90aebce05b3cd07458e2
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-06-19 14:50:44 +0200

    test : readAFile [ 🔴 ROUGE ]

---
+++ src/util/DataManager.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.io.File;
+import java.util.Properties;
+
+public abstract class DataManager {
+    public static Properties readAFile(File File) {
+
+        return null;
+    }
+
+    public static Properties readAFile(String file) {
+        return null;
+    }
+}
---
+++ test/util/DataManagerTest.java
@@ -0,0 +1,24 @@
+package util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class DataManagerTest {
+    @Test
+    public void testReadAFile() {
+        String src = "./ressources/test.conf";
+
+        assertDoesNotThrow(() -> {
+            Properties properties = DataManager.readAFile(src);
+
+            assertNotNull(properties);
+            assertEquals(properties.getProperty("test"), "test");
+        });
+
+    }
+}
