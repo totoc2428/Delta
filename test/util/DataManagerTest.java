f395b5ea116cb4067e4b714dd5bb525715a892a0
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-06-19 15:01:59 +0200

    test : readAFile [ 🔴 Rouge ]

--- test/util/DataManagerTest.java
+++ test/util/DataManagerTest.java
@@ -2,6 +2,8 @@ package util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.Properties;
@@ -11,13 +13,21 @@ import org.junit.Test;
 public class DataManagerTest {
     @Test
     public void testReadAFile() {
-        String src = "./test/ressources/test.conf";
 
         assertDoesNotThrow(() -> {
+            String src = "./test/ressources/test.conf";
             Properties properties = DataManager.readAFile(src);
 
             assertNotNull(properties);
             assertEquals(properties.getProperty("test"), "test");
+        });
+
+        assertThrows(Exception.class, () -> {
+            String src = "";
+
+            Properties properties = DataManager.readAFile(src);
+
+            assertNull(properties);
         });
 
     }
