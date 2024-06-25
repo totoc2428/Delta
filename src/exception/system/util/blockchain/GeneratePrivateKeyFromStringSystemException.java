0db29d01114add16af5766761d0d5a2ed3babc05
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-06-25 11:06:43 +0200

    test : generatePrivateKeyFromString [ 🔴 ROUGE ]

---
+++ src/exception/system/util/blockchain/GeneratePrivateKeyFromStringSystemException.java
@@ -0,0 +1,11 @@
+package exception.system.util.blockchain;
+
+import exception.system.SystemException;
+
+public class GeneratePrivateKeyFromStringSystemException extends SystemException {
+
+    public GeneratePrivateKeyFromStringSystemException() {
+        super("GeneratePrivateKeyFromStringSystemException");
+    }
+
+}
--- src/util/blockchain/BlockchainManager.java
+++ src/util/blockchain/BlockchainManager.java
@@ -1,14 +1,21 @@
 package util.blockchain;
 
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
 import java.util.Properties;
 
 import exception.system.util.blockchain.BlockchainManagerLoadException;
+import exception.system.util.blockchain.GeneratePrivateKeyFromStringSystemException;
 import exception.system.util.data.PropertiesReadingSystemException;
+import java.security.KeyPair;
 import util.data.DataManager;
 import util.primary.Primary;
 
 public abstract class BlockchainManager {
     private static Properties initBlockchainProperties;
+
+    private static
 
     /* -LOADER */
     public static void load() throws BlockchainManagerLoadException {
@@ -25,5 +32,26 @@ public abstract class BlockchainManager {
     /* --GETTER */
     public static Properties getInit() {
         return initBlockchainProperties;
+    }
+
+    /* -GENERATOR */
+    /* --PRIVATE_KEY */
+    public static PrivateKey generatePrivateKeyFromString(String input)
+            throws GeneratePrivateKeyFromStringSystemException {
+        try {
+            SecureRandom random = SecureRandom.getInstance(KEY_RANDOM_GENERATOR_INSTANCE);
+            random.setSeed(input.getBytes());
+
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+            keyGen.initialize(KEY_SIZE, random);
+
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            return keyPair.getPrivate();
+
+        } catch (Exception e) {
+            throw new GeneratePrivateKeyFromStringSystemException();
+        }
+
     }
 }
