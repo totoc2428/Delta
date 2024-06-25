4768ff0ec69cca7eb3cb86e37c00900b60f2808c
charles.coude.etu <charles.coude.etu@univ-lille.fr>
2024-06-25 11:24:20 +0200

    test : generatePrivateKeyFromString [ 🟢 VERT ]

--- src/util/blockchain/BlockchainManager.java
+++ src/util/blockchain/BlockchainManager.java
@@ -15,12 +15,42 @@ import util.primary.Primary;
 public abstract class BlockchainManager {
     private static Properties initBlockchainProperties;
 
-    private static
+    private static String signatureAlgorithm;
+
+    private static String digestAlgorithm;
+
+    private static String keyAlgorithm;
+    private static String KeyRandomGeneratorInstance;
+    private static int keySize;
+    private static int keyExponant;
+
+    private static int encryptorKeySize;
+    private static String encryptorAlgorithm;
+    private static String savedKeyEncryptorSpace;
+
+    /* -Attribut */
+    /* --GETTER */
+    public static String getSavedKeyEncryptorSpace() {
+        return savedKeyEncryptorSpace;
+    }
 
     /* -LOADER */
     public static void load() throws BlockchainManagerLoadException {
         try {
             initBlockchainProperties = DataManager.readAFile(Primary.getBlockchainManagerInitPath());
+
+            signatureAlgorithm = initBlockchainProperties.getProperty("SIGNATURE_ALGORITHM");
+            digestAlgorithm = initBlockchainProperties.getProperty("DIGEST_ALGORITHM");
+
+            keyAlgorithm = initBlockchainProperties.getProperty("KEY_ALGORITHM");
+            KeyRandomGeneratorInstance = initBlockchainProperties.getProperty("KEY_RANDOM_GENERATOR_INSTANCE");
+            keySize = Integer.parseInt(initBlockchainProperties.getProperty("KEY_SIZE"));
+            keyExponant = Integer.parseInt(initBlockchainProperties.getProperty("KEY_EXPONANT"));
+
+            encryptorKeySize = Integer.parseInt(initBlockchainProperties.getProperty("ENCRYPTOR_KEY_SIZE"));
+            encryptorAlgorithm = initBlockchainProperties.getProperty("ENCRYPTOR_ALGORITHM");
+            savedKeyEncryptorSpace = initBlockchainProperties.getProperty("SAVED_KEY_ENCRYPTOR_SPACE");
+
         } catch (PropertiesReadingSystemException e) {
             e.show();
 
@@ -39,11 +69,11 @@ public abstract class BlockchainManager {
     public static PrivateKey generatePrivateKeyFromString(String input)
             throws GeneratePrivateKeyFromStringSystemException {
         try {
-            SecureRandom random = SecureRandom.getInstance(KEY_RANDOM_GENERATOR_INSTANCE);
+            SecureRandom random = SecureRandom.getInstance(KeyRandomGeneratorInstance);
             random.setSeed(input.getBytes());
 
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
-            keyGen.initialize(KEY_SIZE, random);
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(keyAlgorithm);
+            keyGen.initialize(keySize, random);
 
             KeyPair keyPair = keyGen.generateKeyPair();
 
--- test/util/blockchain/BLockchainManagerTest.java
+++ test/util/blockchain/BLockchainManagerTest.java
@@ -1,5 +1,33 @@
 package util.blockchain;
 
-public class BLockchainManagerTest {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.security.PrivateKey;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import exception.system.util.blockchain.BlockchainManagerLoadException;
+
+public class BLockchainManagerTest {
+    @Before
+    public void initBlockchainManagerTest() {
+        try {
+            BlockchainManager.load();
+        } catch (BlockchainManagerLoadException e) {
+            e.show();
+        }
+    }
+
+    @Test
+    public void testGeneratePrivateKeyFromString() {
+        assertDoesNotThrow(() -> {
+            String str = "truc";
+
+            PrivateKey privateKey = BlockchainManager.generatePrivateKeyFromString(str);
+            assertNotNull(privateKey);
+        });
+
+    }
 }
