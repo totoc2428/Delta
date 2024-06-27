910b6970fa6bdd80f238e12fc9b702c9af71140e
Charles Coudé <charles.coude.etu@univ-lille.fr>
2024-06-27 11:09:23 +0200

    ajout de chainObject ( objet parent )

---
+++ src/model/entity/ChainObject.java
@@ -0,0 +1,22 @@
+package model.entity;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public abstract class ChainObject {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public ChainObject(PrivateKey privateKey, PublicKey publicKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+}
