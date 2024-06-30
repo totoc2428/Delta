1b3198650beb3222e6ba61fda8ee933da71587e5
Charles Coudé <charles.coude.etu@univ-lille.fr>
2024-06-30 02:05:34 +0200

    ajout du save

---
+++ src/exception/system/model/manager/chainobject/SaveChainobjectSystemException.java
@@ -0,0 +1,11 @@
+package exception.system.model.manager.chainobject;
+
+import exception.system.model.manager.ManagerSystemException;
+
+public class SaveChainobjectSystemException extends ManagerSystemException {
+
+    public SaveChainobjectSystemException() {
+        super("SaveChainobjectSystemException");
+    }
+
+}
--- src/model/manager/ChainObjectManager.java
+++ src/model/manager/ChainObjectManager.java
@@ -4,14 +4,22 @@ import java.util.Properties;
 
 import exception.system.model.manager.chainobject.ChainObjectManagerLoadSystemException;
 import exception.system.model.manager.chainobject.ChainObjectManagerSetInitFilePathSystemException;
+import exception.system.model.manager.chainobject.SaveChainobjectSystemException;
+import exception.system.util.blockchain.PrivateKeyToSavedFormatSystemException;
+import exception.system.util.blockchain.PublicKeyToSavedFormatSystemException;
 import exception.system.util.data.PropertiesReadingSystemException;
+import exception.system.util.security.EncryptSecurityManagerSystemException;
+import model.entity.ChainObject;
+import util.blockchain.BlockchainManager;
 import util.data.DataManager;
 import util.primary.Primary;
+import util.security.SecurityManager;
 
 public abstract class ChainObjectManager {
     private static Properties initChainObjectManager;
 
     private static String chainObjectSavedTag;
+    private static String savedObjectTypeKey;
 
     /* -LOADER */
     public static void load() throws ChainObjectManagerLoadSystemException {
@@ -25,7 +33,7 @@ public abstract class ChainObjectManager {
         }
         if (initChainObjectPropertiesCheck()) {
             chainObjectSavedTag = initChainObjectManager.getProperty("CHAINOBJECT_SAVED_TAG");
-
+            savedObjectTypeKey = initChainObjectManager.getProperty("SAVED_OBJECT_TYPE_KEY");
         } else {
             throw new ChainObjectManagerLoadSystemException();
         }
@@ -45,7 +53,8 @@ public abstract class ChainObjectManager {
 
     /* --CHEKER */
     private static boolean initChainObjectPropertiesCheck() {
-        return checkAChainObjectProperty("CHAINOBJECT_SAVED_TAG");
+        return checkAChainObjectProperty("CHAINOBJECT_SAVED_TAG") &&
+                checkAChainObjectProperty("SAVED_OBJECT_TYPE_KEY");
     }
 
     private static boolean checkAChainObjectProperty(String primaryCode) {
@@ -53,5 +62,27 @@ public abstract class ChainObjectManager {
                 .getProperty(
                         primaryCode)
                 .isBlank() && !initChainObjectManager.getProperty(primaryCode).isEmpty()));
+    }
+
+    /* -SAVE */
+    public static Properties save(ChainObject chainObject) throws SaveChainobjectSystemException {
+        Properties chainObjectProperties = new Properties();
+
+        chainObjectProperties.setProperty(savedObjectTypeKey, chainObjectSavedTag);
+        try {
+            chainObjectProperties.setProperty("privatekey",
+                    SecurityManager.encrypt(BlockchainManager.privateKeyToSavedFormat(chainObject.getPrivateKey()),
+                            chainObject.getPublicKey()));
+
+            chainObjectProperties.setProperty("publickey",
+                    BlockchainManager.publicKeyToSavedFormat(chainObject.getPublicKey()));
+
+        } catch (EncryptSecurityManagerSystemException | PrivateKeyToSavedFormatSystemException
+                | PublicKeyToSavedFormatSystemException e) {
+            e.show();
+            throw new SaveChainobjectSystemException();
+        }
+
+        return chainObjectProperties;
     }
 }
